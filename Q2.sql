ecda24b43b398c1c371f2e8e3e6cb445ca5770f3
Isuri Wijesinghe Gunaratne <75952849+IsuriGunaratne@users.noreply.github.com>
2024-08-14 17:27:18 +0530

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,1 +1,43 @@
+CREATE TABLE IF NOT EXISTS students(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  student_name VARCHAR(100) NOT NULL,
+  contact VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS course(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  course_name VARCHAR(100) NOT NULL,
+  duration_in_months INT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS lecturer(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  lecturer_name VARCHAR(100) NOT NULL,
+  contact VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS lecturer_course(
+  lecturer_id INT,
+  course_id INT,
+  CONSTRAINT PRIMARY KEY (lecturer_id, course_id),
+  CONSTRAINT fk_1 FOREIGN KEY (lecturer_id) REFERENCES lecturer_id,
+  CONSTRAINT fk_2 FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
